35d2b54a171a106fc0a133902ee118e23f0a24da
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-10 17:15:36 +0530

    Added : Initials files required for the program

---
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.emailvalidation;
+
+public class EmailValidation 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Email Validation");
+	}
+}
