74e5e7e8b1c7dd7f2bdcd03031fa195cfb2aeeff
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-10 18:28:59 +0530

    Added : Method to validate domain name along with @

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -6,12 +6,30 @@ public class EmailValidation
 {
 	public static void main(String[] args) 
 	{
-		EmailValidation emailCheck = new EmailValidation();
-
 		System.out.println("Welcome to Email Validation");
-		String str = "abc";
+		//uc1 - email should start with letters or number
+		EmailValidation emailCheck1 = new EmailValidation();
+		String str1 = "abc";
+		emailCheck1.checkFirstLetter(str1);
+				
+		//uc2 - Ensure @ and validate the mandatory 2nd part
+		EmailValidation emailCheck2 = new EmailValidation();
+		String str2 = "@bridgelabz";
+		emailCheck2.checkDomainName(str2);
+	}
 
-		emailCheck.checkFirstLetter(str);
+	//check email should start @ and have domain name
+	private void checkDomainName(String str2)
+	{
+		boolean isMatched = Pattern.compile("^@[a-zA-Z]+").matcher(str2).matches();
+		if (isMatched) 
+		{			
+			System.out.println("Domain name is correct");
+		}
+		else
+		{			
+			System.out.println("Domain name is not correct");
+		}
 	}
 
 	//check email should start with letters or number 
