a9ac37aba1df195b448edc3c9f62af09db641aec
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-10 18:06:26 +0530

    Added : Method to check email starts with letters or number

--- src/com/bridgelabz/emailvalidation/EmailValidation.java
+++ src/com/bridgelabz/emailvalidation/EmailValidation.java
@@ -1,9 +1,30 @@
 package com.bridgelabz.emailvalidation;
 
+import java.util.regex.Pattern;
+
 public class EmailValidation 
 {
 	public static void main(String[] args) 
 	{
+		EmailValidation emailCheck = new EmailValidation();
+
 		System.out.println("Welcome to Email Validation");
+		String str = "abc";
+
+		emailCheck.checkFirstLetter(str);
+	}
+
+	//check email should start with letters or number 
+	private void checkFirstLetter(String str)
+	{
+		boolean isMatched = Pattern.compile("^[0-9a-zA-Z]+").matcher(str).matches();
+		if (isMatched) 
+		{			
+			System.out.println("Initials letters are correct");
+		}
+		else
+		{			
+			System.out.println("Initials letters are not correct");
+		}
 	}
 }
